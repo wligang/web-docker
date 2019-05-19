15527390ecda003f70179c177dc8f2fbf00d2c5e
Ligang.Wang <wlgchun@163.com>
2019-05-19 19:07:47 +0800

    validate auto commit to load docker image to aliyun`s RC

--- src/main/java/com/wlgdo/avatar/docker/web/IndexController.java
+++ src/main/java/com/wlgdo/avatar/docker/web/IndexController.java
@@ -3,6 +3,7 @@ package com.wlgdo.avatar.docker.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +21,12 @@ public class IndexController {
         logger.info("This is atest:{}");
         return "welcome to Hido";
     }
+
+    @GetMapping("user/{userName}")
+    public Object getUuser(@PathVariable String userName) {
+        logger.info("Welcome to you :[{}]", userName);
+        return String.format("welcome to Hido,your login UserName is[%s]", userName);
+    }
+
+
 }
