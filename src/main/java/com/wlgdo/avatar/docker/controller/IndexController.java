173ab191875ca342bbd13758dc96c2039fb74de9
Ligang.Wang <wlgchun@163.com>
2019-05-30 23:57:36 +0800

    delete no used file

--- src/main/java/com/wlgdo/avatar/docker/controller/IndexController.java
+++ src/main/java/com/wlgdo/avatar/docker/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.wlgdo.avatar.docker.web;
+package com.wlgdo.avatar.docker.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
---
+++ src/test/java/com/wlgdo/avatar/docker/test.java
@@ -0,0 +1,19 @@
+package com.wlgdo.avatar.docker;
+
+
+import static java.lang.Math.log;
+import static java.lang.Math.log10;
+
+/**
+ * Author: Ligang.Wang[wlgchun@l63.com]
+ * Date: 2019/5/25 15:20
+ */
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println(log10(100));
+
+
+
+    }
+}
